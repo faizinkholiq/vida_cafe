5125cc37372c9f41f419897269ef2dd081c32ea5
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-05-01 11:58:37 +0700

    update filter reservation

--- application/views/system/reservation.php
+++ application/views/system/reservation.php
@@ -14,6 +14,19 @@
 .approve-btn.refused{
     color: #ff3f34;
 }
+
+.btn-secondary{
+    background: #eee;
+    color: #666;
+    font-weight: bold;
+    border: none;
+    font-size: 0.8rem;
+}
+
+.btn-secondary:hover{
+    background: #ddd;
+    color: #666;
+}
 </style>
 <div class="app-page-title">
     <div class="page-title-wrapper">
@@ -34,6 +47,26 @@
             <div class="card-header">List reservation
             </div>
             <div class="table-responsive">
+                <div class="row">
+                    <div class="col-md-12 mb-2 " style="
+                        display: flex;
+                        padding: 1rem;
+                        margin-left: 1rem;
+                    ">
+                    <input name="name" id="tbBookID" placeholder="Search by Book ID" type="text" class="form-control col-md-3" style="
+                        border-top-right-radius: 0;
+                        border-bottom-right-radius: 0;
+                    " required>
+                    <button type="button" class="btn btn-primary" onclick="searchAction()" style="
+                        border-top-left-radius: 0;
+                        border-bottom-left-radius: 0;
+                    "><i class="fa fa-search"></i></button>
+
+                    <button type="button" class="btn btn-secondary " onclick="clearFilter()" style="
+                        margin-left: 1.5rem;
+                    "><i class="fa fa-list" style="margin-right:0.5rem"></i>All Data</button>
+                    </div>
+                </div>
                 <table class="align-middle mb-0 table table-borderless table-striped table-hover">
                     <thead>
                         <tr>
@@ -46,7 +79,7 @@
                             <th class="text-center">Approved</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody id="reservation-container">
                     <?php $num = 1; foreach($data as $key => $value): ?>
                         <tr>
                             <td class="text-center text-muted"><?=$num++ ?></td>
@@ -71,6 +104,7 @@
 <script>
     let data = <?=json_encode($data) ?>;
     let base = '<?=base_url() ?>';
+    let site = '<?=site_url() ?>';
 
     function showModal(modal, show){
         if(show){
@@ -100,5 +134,44 @@
     function deleteAction(id){
         $('#deleted_id').val(id);
         showModal('#deleteModal', true)
+    }
+
+    function searchAction(){
+        let txt = $('#tbBookID').val();
+
+        if(txt !== null || txt !== ""){
+            let row = data.filter(a => a.code.includes(txt))
+            loadData(row);
+        }
+    }
+
+    function clearFilter(){
+        let txt = $('#tbBookID').val('');
+        loadData(data);
+    }
+
+    function loadData(data){
+        $.when($('#reservation-container').fadeOut(100)).done(function(){
+            $('#reservation-container').html('');
+            data.forEach(function(r, i){
+                r.status = r.status === null? '': r.status;
+    
+                $('#reservation-container').append(`
+                <tr>
+                        <td class="text-center text-muted">${i+1}</td>                   
+                        <td>${r.code}</td>
+                        <td>${r.time}</td>
+                        <td>${r.name}</td>
+                        <td class="text-center">${r.people}</td>
+                        <td>${r.contact}</td>
+                        <td class="text-center">
+                            <a data-toggle="tooltip" title="Approved" href="${site + "admin/reservation/approved/" +r.id + "/1" }"><i class="fa fa-check fa-w-20 btn-icon-wrapper approve-btn ${r.status.toLowerCase() === "approved"? "approved" : ""}"> </i></a>
+                            <a data-toggle="tooltip" title="Refused" href="${site + "admin/reservation/approved/" +r.id + "/1" }"><i class="fa fa-times fa-w-20 btn-icon-wrapper approve-btn ${r.status.toLowerCase() === "refused"? "refused" : ""}"> </i></a>
+                        </td>
+                    </tr>
+                `).hide().fadeIn('100');
+            });
+        });
+
     }
 </script>
